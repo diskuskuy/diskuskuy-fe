2e776db1090c98e6f1b5617462db1a8cdd12eeeb
nadilatusifa <nadilatusifa83@gmail.com>
2023-04-03 14:15:02 +0700

    create-post untuk reply dan nested reply

--- src/pages/create-post.js
+++ src/pages/create-post.js
@@ -1,32 +1,85 @@
-import { Editor } from '@tinymce/tinymce-react';
-export default function CreatePost() {
-    const editorRef = useRef(null);
-    return (
-    <>
-      <Editor
-        apiKey="32ne23x277g7cdbgy01eos5p0f9hlaz55et1nzrf4rra50dl"
-        onInit={(evt, editor) => (editorRef.current = editor)}
-        init={{
-          height: 500,
-          menubar: false,
-          plugins:
-            "advlist autolink lists link image charmap preview anchor searchreplace visualblocks insertdatetime media table code help wordcount fullscreen emoticons codesample",
-          toolbar:
-            "undo redo | formatselect | " +
-            "bold italic backcolor | alignleft aligncenter alignright alignjustify |" +
-            "bullist numlist outdent indent | image media emoticons link codesample charmap hr |" +
-            "removeformat | help",
-          content_style:
-            "body { font-family:Helvetica,Arial,sans-serif; font-size:14px }",
-          file_picker_types: "image media",
-          file_picker_callback: function (callback, value, meta) {
-            console.log(value);
-            console.log(meta);
-            
-          },
-        }}
-      />
-    </>
-  )
+import TextEditor from "@/components/Forum/TextEditor";
+import React, { useRef, useState, ChangeEvent } from "react";
+import styles from "@/styles/CreateForum.module.css";
+import ChevronRightIcon from "@mui/icons-material/ChevronRight";
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import FormControl from "@mui/material/FormControl";
+import Select, { SelectChangeEvent } from "@mui/material/Select";
+import { useRouter } from "next/router";
 
+export default function CreatePost() {
+  const editorRef = useRef(null);
+  const router = useRouter();
+  const [tags, setTags] = React.useState([]);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(tags);
+    if (editorRef.current) {
+      console.log(editorRef.current.getContent());
+    }
+    if(editorRef.current.getContent().length>0 && tags.length>0){
+      router.push('/forum/1')
+    }
+  };
+  const handleChange = (event) => {
+    const {
+      target: { value },
+    } = event;
+    setTags(
+      // On autofill we get a stringified value.
+      typeof value === "string" ? value.split(",") : value
+    );
+    console.log(event);
+  };
+  const names = ["Pertanyaan", "Pendapat", "Bingung"];
+  return (
+    <>
+      <main className={styles.main}>
+        <div className="flex flex-row items-center text-xs pb-10">
+          <a className="cursor-pointer">Sistem Interaksi - Gasal 2020/2021</a>
+          <ChevronRightIcon />
+          <a className="cursor-pointer">Forum Diskusi Minggu ke-1</a>
+        </div>
+        <div className="text-center text-2xl font-bold">
+          <h2>Tulis Postingan</h2>
+        </div>
+        <form onSubmit={handleSubmit}>
+          <TextEditor editorRef={editorRef}></TextEditor>
+
+          <div className="mt-5">
+            {/* <FormControl sx={{ m: 1, width: 300 }}> */}
+            <InputLabel id="demo-multiple-name-label">Tags</InputLabel>
+            <Select
+              className="bg-white"
+              labelId="demo-multiple-name-label"
+              id="demo-multiple-name"
+              multiple
+              value={tags}
+              onChange={handleChange}
+            >
+              {names.map((name) => (
+                <MenuItem key={name} value={name}>
+                  {name}
+                </MenuItem>
+              ))}
+            </Select>
+            {/* </FormControl> */}
+            <div className="flex flex-row-reverse gap-2">
+              <input
+                type="submit"
+                value="Simpan"
+                className="bg-[#2ECC71] text-white p-2 rounded cursor-pointer"
+              />
+              <input
+                value="Batal"
+                className="bg-[#FFFFFF] text-black p-2 rounded cursor-pointer w-16"
+              />
+            </div>
+          </div>
+        </form>
+      </main>
+    </>
+  );
 }
