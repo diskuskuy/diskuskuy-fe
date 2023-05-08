3dbe23c8372435e78c28a9ad89eaba905c34e9fb
Sonia Rahmawati <soniarahma27@gmail.com>
2023-05-08 13:03:49 +0700

    edit post

---
+++ src/api/edit-reply-api.js
@@ -0,0 +1,54 @@
+export const editPost = async (requestBody, id) => {
+    try {
+      const response = await fetch(
+        `${process.env.NEXT_PUBLIC_BE_URL}/post/post/${id}/`,
+        {
+          method: "PUT",
+          headers: {
+            "Authorization": `Token ${localStorage.getItem("token")}`,
+            "Content-Type": "application/json",
+          },
+          body: requestBody,
+        }
+      );
+  
+      if (!response.ok) {
+        const responseError = await response.json();
+        const message = `${responseError.errors.error_message}`;
+        throw new Error(message);
+      }
+      const responseData = await response.json();
+      return responseData;
+    } catch (error) {
+      // toast.error(error.message)
+      console.log(error.message);
+    }
+  };
+
+  
+  export const fetchPost = async (id) => {
+    try {
+      const response = await fetch(
+        `${process.env.NEXT_PUBLIC_BE_URL}/post/post/${id}/`,
+        {
+          method: "GET",
+          headers: {
+            "Authorization": `Token ${localStorage.getItem("token")}`,
+          }
+        }
+      );
+  
+      if (!response.ok) {
+        const responseError = await response.json();
+        const message = `${responseError.errors.error_message}`;
+        throw new Error(message);
+      }
+      const responseData = await response.json();
+      return responseData;
+    } catch (error) {
+      // toast.error(error.message)
+      console.log(error.message);
+    }
+  };
+
+  
